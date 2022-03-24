0afbf5f1cd5da32e723907256e56e171f1446923
Vovka <70479034+vovka8101@users.noreply.github.com>
2022-03-24 14:02:23 +0200

    Delete find image; Add new definition and examples for findWord.py

--- findWord.py
+++ findWord.py
@@ -1,65 +1,62 @@
 import requests
-import os
 from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
-import matplotlib.image as mpim
-import shutil
 from datetime import datetime
 
 today = datetime.now().strftime('%d-%m-%y')
 fname = "./daily_words/new_words" + today + ".txt"
+fname_all = "./daily_words/all_words.txt"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
 def findWord():
     global fname
     while True:
+        explain = ""
         word = input("Enter a word ('--q' for exit): ").lower()
         if word == '--q':
             break
         url = "https://dictionary.cambridge.org/dictionary/english/" + word.replace(' ', '+')
+        url2 = "https://wordsinasentence.com/" + word.replace(' ', '-') + "-in-a-sentence/"
         try:
             print("\nProccess ...\n")
             response = requests.get(url, headers=headers)
+            response2 = requests.get(url2, headers=headers)
             soup = BeautifulSoup(response.text, 'lxml')
-            quotes = soup.find('div', class_='dimg')
-
+            soup2 = BeautifulSoup(response2.text, 'lxml')
+            examples = soup2.find('div', class_='thecontent clearfix')
             sentences = soup.find_all('div', class_='def ddef_d db')
             i = 0
-            for sentence in sentences:
-                i += 1
-                print(f"{i}. {sentence.text}")
+            if examples:
+                for example in examples:
+                    i += 1
+                    example = example.text.rstrip()
+                    if not example:
+                        continue
+                    if example.find('PREV WORD') != -1:
+                        break
+                    elif example.find('\n') != -1:
+                        example = example[:example.find('\n')]
+                    print(example)
+                    explain += example + '@'
+            elif sentences:
+                for sentence in sentences:
+                    i += 1
+                    print(f"{i}. {sentence.text}")
+                    explain += f"{i}. {sentence.text}@"
 
             if i == 0:
-                print("Sentences not found ;(\n")
+                print("Not found ;(\n")
                 continue
             else:
                 print("Success :)")
-
-            print("\nTry to find an image ...")
-            images = quotes.find('amp-img')
-            img_link = "https://dictionary.cambridge.org/" + images.attrs['src']
-            r = requests.get(img_link, headers=headers, stream=True)
-            if r.status_code == 200:
-                with open(f"{word}.jpg", 'wb') as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
-            ok = True
-        except (ConnectionError, AttributeError):
-            print("Image not found ;(\n")
-            ok = False
-
-        if ok:
-            print("Success :)\n")
-            img = mpim.imread(f"{word}.jpg")
-            plt.imshow(img)
-            plt.show()
-            os.remove(f"{word}.jpg")
+        except ConnectionError:
+            print("Connection Error ;(\n")
 
         translate = input("Add Ukrainian translate ('--n' - don't save): ").lower()
         if translate.find("--n") == -1:
-            with open(fname, 'a', encoding='utf-8') as f:
-                f.write(f"{word} = {translate}\n")
+            with open(fname, 'a', encoding='utf-8') as f, open(fname_all, 'a', encoding='utf-8') as f2:
+                f.write(f"{word} = {translate} = {explain}\n")
+                f2.write(f"{word} = {translate} = {explain}\n")
 
 
 if __name__ == "__main__":
